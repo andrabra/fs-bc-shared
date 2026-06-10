f7d1024ca08b3ab62ea0475d55d88ea268aacf0d
Андрей Майоров <majorov.ak@modernglass.ru>
2026-06-10 15:42:36 +0500

    Закрыл задачу с подсчетом самых частых элементов массива

--- solutions/fa/19.js
+++ solutions/fa/19.js
@@ -3,7 +3,27 @@
 // Гарантируется, что массив не пустой.
 
 function mostFrequent(arr) {
-  // TODO
+  const mapCounter = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (mapCounter.has(arr[i])) {
+      mapCounter.set(arr[i], mapCounter.get(arr[i]) + 1);
+    } else {
+      mapCounter.set(arr[i], 1);
+    }
+  }
+
+  let max = -Infinity;
+  let maxKey = null;
+
+  for ([key, value] of mapCounter) {
+    if (value > max) {
+      max = value;
+      maxKey = key;
+    }
+  }
+
+  return maxKey;
 }
 
 console.log(mostFrequent([1, 2, 2, 3]));
